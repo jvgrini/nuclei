5730f7e80d9323d191c251cb8ef5c30c56cfedd3
Jonas Grini <jonas@stolav-dhcp-10-20-87-113.wlan.ntnu.no>
2024-01-10 11:41:57 +0100

    cleaned up folder

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ mask_region_folder = "masks_regions_extended"
 
 
 
-ipsii_pairs, contra_pairs, ipsii_names, contra_names = mp.load_images_and_masks(image_folder, mask_folder)
+# ipsii_pairs, contra_pairs, ipsii_names, contra_names = mp.load_images_and_masks(image_folder, mask_folder)
 
-ipsii_properties, contra_properties = mp.measure_properties(ipsii_pairs, contra_pairs)
+# ipsii_properties, contra_properties = mp.measure_properties(ipsii_pairs, contra_pairs)
 
-for i in range(len(ipsii_properties)):
-    mp.classify_neurons(ipsii_pairs[i][1],ipsii_properties[i], ipsii_names[i])
-for i in range(len(contra_properties)):
-    mp.classify_neurons(contra_pairs[i][1],contra_properties[i], contra_names[i])
+# for i in range(len(ipsii_properties)):
+#     mp.classify_neurons(ipsii_pairs[i][1],ipsii_properties[i], ipsii_names[i])
+# for i in range(len(contra_properties)):
+#     mp.classify_neurons(contra_pairs[i][1],contra_properties[i], contra_names[i])
 
-#mp.plot_g4_whole_image(ipsii_properties, contra_properties)
+# mp.plot_g4_whole_image(ipsii_properties, contra_properties)
 
-#ipsii_properties, contra_properties = mp.load_images_masks_and_regionmasks(image_folder, mask_folder, mask_region_folder)
-#mp.measure_channel_regions(ipsii_properties, contra_properties)
-#mp.measure_region_nuclei(ipsii_properties, contra_properties)
+ipsii_properties, contra_properties = mp.load_images_masks_and_regionmasks(image_folder, mask_folder, mask_region_folder)
+mp.measure_channel_regions(ipsii_properties, contra_properties)
+mp.measure_region_nuclei(ipsii_properties, contra_properties)
